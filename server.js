cb1e50108d5574cdec7f61bc77bff647acdde6a2
ekhwannul <g-89414799@moe-dl.edu.my>
2025-10-05 23:51:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ Topik Pengajaran: "${tajuk}"
 Fokus Kemahiran (Standard Pembelajaran): "${sp}"
 
 Syarat:
-- Hasilkan antara 5 hingga 7 langkah pengajaran yang logik, bergantung pada kesesuaian aktiviti. jangan lebih dari 7 langkah aktiviti, 7 paling banyak.
+- Hasilkan antara 5 hingga 7 langkah pengajaran yang logik, bergantung pada kesesuaian aktiviti. jangan lebih dari 7 langkah aktiviti, 7 paling banyak. Ringkaskan sikit ayat jangan ayat panjang dan kompleks sangat tapi jangan ringkas sangat, ringkas tapi padat.
 - Langkah terakhir WAJIB "Guru dan murid membuat refleksi tentang pengajaran hari ini.".
 - Jangan sertakan "Set Induksi".
 - Berikan jawapan dalam format senarai bernombor.
