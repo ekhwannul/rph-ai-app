f42a6a00f1833dc401726347880ebc45486452d2
ekhwannul <g-89414799@moe-dl.edu.my>
2025-10-06 19:10:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ Topik Pengajaran: "${tajuk}"
 Fokus Kemahiran (Standard Pembelajaran): "${sp}"
 
 Syarat:
-- Hasilkan antara 5 hingga 7 langkah pengajaran yang logik, bergantung pada kesesuaian aktiviti.  
+- Hasilkan antara 5 langkah pengajaran yang logik, bergantung pada kesesuaian aktiviti. (JANGAN LEBIH DARI LIMA)
 - Bina ayat yang ringkas sahaja untuk setiap aktiviti 
 - Langkah terakhir WAJIB "Guru dan murid membuat refleksi tentang pengajaran hari ini.".
 - Jangan sertakan "Set Induksi".
